2a9a0cfa6dec077ae3fec3c9f551f3792e953367
Daorss <149414829+Daorss@users.noreply.github.com>
2023-12-09 16:21:11 +0100

    Add files via upload

--- valid.js
+++ valid.js
@@ -5,10 +5,13 @@ var password = document.forms['form']['password'];
 var email_error = document.getElementById('email_error');
 var pass_error = document.getElementById('pass_error');
 
-email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', pass_Verify);
+email.addEventListener('input', email_Verify);
+password.addEventListener('input', pass_Verify);
+
+
 
 function validated(){
+	var emailValue = email.value.trim();
 	if (email.value.length < 4 || !isValidEmail(emailValue)) {
 		email.style.border = "1px solid red";
 		email_error.style.display = "block";
@@ -23,6 +26,11 @@ function validated(){
 	}
 
 }
+
+function isValidEmail(email) {
+	return /\S+@\S+\.\S+/.test(email);
+}
+
 function email_Verify(){
 	var emailValue = email.value.trim();
     if (email.value.length >= 4 && isValidEmail(emailValue)) {
@@ -31,11 +39,11 @@ function email_Verify(){
         return true;
     }
 }
-function pass_Verify(){
-	if (password.value.length >= 5) {
-		password.style.border = "1px solid silver";
-		pass_error.style.display = "none";
-		return true;
-	}
+function pass_Verify() {
+    if (password.value.length >= 7) {
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
 }
 
