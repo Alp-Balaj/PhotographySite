381f2beee751cbbb3efd5c02ce37afc3bc757469
Daorss <149414829+Daorss@users.noreply.github.com>
2024-02-12 14:16:53 +0100

    Add files via upload

--- ProjektPjesa2/connections.php
+++ ProjektPjesa2/connections.php
@@ -1,15 +1,42 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "projektdb";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!class_exists('Database')) {
+    class Database {
+        private $servername = "localhost";
+        private $username = "root";
+        private $password = "";
+        private $dbname = "projektdb";
+        private $conn;
+    
+        public function __construct() {
+            $this->connect();
+        }
+    
+        private function connect() {
+            $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+    
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+        }
+    
+        public function getConnection() {
+            return $this->conn;
+        }
+    
+        public function __destruct() {
+            if ($this->conn) {
+                $this->conn->close();
+            }
+        }
+    }
 }
+
+
+
+$database = new Database();
+
+$conn = $database->getConnection();
+
 
 ?>
